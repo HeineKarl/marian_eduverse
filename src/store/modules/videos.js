37c3fcfb3c3031355d219be8ad97284d0abbae41
Your NameKarl2805 <karl20vincent00@gmail.com>
2022-12-12 11:00:09 +0800

    Added Information

--- src/store/modules/videos.js
+++ src/store/modules/videos.js
@@ -9,12 +9,21 @@ export const videos = {
         description:
           "This video is all about colors and its appropriateness in design. It is important to note that colors have different meanings for everyone and thinking about not seeing it correctly might change your perspective towards color picking. With this, the video will showcase awareness for colors that is at the same time appropriate and maintaining your design better.",
         link: "https://drive.google.com/file/d/1_ubgEdoyopREBV-ZRIUpS_wJBl18r1VK/preview",
-        creators: ["Karl Vincent Del Rosario", "Maurice Bernardino"],
+        creators: [
+          "Karl Vincent Del Rosario",
+          "Maurice Bernardino",
+          "Patricia Lenon",
+          "Francescine Ian Dela Rosa",
+          "Edward Tamayo",
+          "Marycel Pacaldo",
+          "Juan Carlo Villena",
+          "Mark Morante",
+        ],
       },
       {
         title: "Job Interview",
         description:
-          "This video would talk about on what would you expect before, during and after an interview. It wyoudl also showcase tips that might help increase the changes of you being hired by the interviewee.",
+          "This video would talk about on what would you expect before, during and after an interview. It wyoudl also showcase tips that might help increase the changes of you being hired by the interviewee. And what behaviors such as aneurysm that one should avoid and necessary documents to bring before the meeting.",
         link: "https://drive.google.com/file/d/1pGqp4TvnlCxGWpDCustyDFGZ6mST55T4/preview",
         creators: [
           "Adrian Cedo",
